3911d85576cd38cf7ed8afcf3b32b4d71d683cf9
MinjaeKimmm <0208mjkim@gmail.com>
2024-05-25 23:14:47 +0000

    Updated Dashboard

--- server/src/routes/search.js
+++ server/src/routes/search.js
@@ -5,8 +5,17 @@ const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
     try {
-      const equipment = await prisma.equipment.findMany();
-      res.json(equipment);
+      const gymID = parseInt(req.query.gymID, 10);
+      if (isNaN(gymID)) {
+        return res.status(400).json({ error: "Invalid gymID" });
+      }
+      const equipments = await prisma.gymEquipment.findMany({
+        where: {
+          gymID: gymID
+        }
+      });
+      let equipmentIDList = equipments.map(equipment => equipment.equipmentID);
+      res.json(equipmentIDList);
     } catch (error) {
       res.status(500).json({ error: "Something went wrong" });
     }
