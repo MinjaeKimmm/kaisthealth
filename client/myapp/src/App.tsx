1dcfffec9b575e7d20f39f2ca880cebd2b371861
MinjaeKimmm <0208mjkim@gmail.com>
2024-05-26 17:37:17 +0000

    Connected frontend to backend for UserWeek

--- client/myapp/src/App.tsx
+++ client/myapp/src/App.tsx
@@ -9,7 +9,6 @@ import Signup from './pages/Signup'
 import Equipments from './pages/Equipments';
 import Dashboard from './pages/Dashboard';
 import UserWeek from './pages/UserWeek';
-import UserWeekTemp from './pages/UserWeekTemp'
 import Auditorium from './pages/gyms/Auditorium';
 import Heemang from './pages/gyms/Heemang';
 import Mir from './pages/gyms/Mir';
@@ -27,7 +26,6 @@ const App = () => {
         <Route path="/equipments" element={<Equipments />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/userweek" element={<UserWeek/>} />
-        <Route path="/userweektemp" element={<UserWeekTemp/>} />
         <Route path="/auditorium" element={<Auditorium />} />
         <Route path="/heemang" element={<Heemang />} />
         <Route path="/mir" element={<Mir />} />
